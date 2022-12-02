cf0ab8f3fdc7777935c694c79077b40af58629c4
Milligani21 <milligani21@yahoo.com>
2022-12-01 19:47:44 -0500

    added concept art and some mumbo-jumbo JS

---
+++ script.js
@@ -0,0 +1,15 @@
+var counter = 0
+while (counter < 25) {
+    console.log (counter)
+    counter++
+}
+
+while (counter === 25) {
+    counter = counter*counter
+}
+
+console.log (counter)
+
+if (counter > 600) {
+    console.log ("Holy cannoli, Thats craaaaazy")
+}
